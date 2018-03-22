0d1efc2537fa342eeb410b3d75bd3aff7b8daff9
Ashish Nandan Singh <ashishcodes4@gmail.com>
2018-03-22 08:18:36 +0530

    events testing

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ d3.selectAll('li')
     .style('font-size', function() {
         return Math.random() * 50 +'px';
     })
-    .style('background-color', '#00feab');
+    .style('background-color', function(_, idx) {
+        return idx % 2 === 0 ? '#00feab' : '#00efab';
+    });
+
+d3.selectAll('li')
+    .style('margin-bottom', '.1em') ;
+
