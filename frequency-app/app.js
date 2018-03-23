5fa42d8c7b94adc63fac1d2baffb20658321f893
Ashish Nandan Singh <ashishcodes4@gmail.com>
2018-03-23 06:20:01 +0530

    updating app.js

--- frequency-app/app.js
+++ frequency-app/app.js
@@ -1,6 +1,13 @@
 // write your code here!
 
 
-function getfrequency(str) {
-    console.log('fron the js file');
+function getFrequency(str) {
+    var sorted  = str.split("").sort();
+    var data = [];
+    for (var i=0; i<sorted.length;i++) {
+        var last = data[data.length-1];
+        if (last && last.character === last[i]) last ++;
+        else data.push({character: sorted[i], count: 1});
+    }
+    return data;
 }
